398dc9bae6158ac282526e0fadac739896699d47
Loc Do <doloccit@gmail.com>
2023-06-17 17:55:41 +0700

    Init vue.

---
+++ components/hello.vue
@@ -0,0 +1,23 @@
+<style>
+.red {
+    color: #f00;
+}
+</style>
+
+<template>
+    <h1 class="red">{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+    props: ['name'],
+    data() {
+        return {
+            msg: `Hello world!`
+        }
+    },
+    mounted() {
+        this.msg = `Hello ${this.name}!`
+    }
+}
+</script>
